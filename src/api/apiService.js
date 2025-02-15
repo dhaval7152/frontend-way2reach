d081cb13d80a429aa8fcb5a38963d465748628c2
dhaval saxena <dp90440@gmail.com>
2025-02-15 15:51:29 +0530

    update endpoints

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -3,16 +3,19 @@ import axios from "axios";
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const getRoles = async () => {
-  const response = await axios.get(`${API_BASE_URL}/roles/getRoles`);
+  const response = await axios.get(`${API_BASE_URL}/api/roles/getRoles`);
   return response.data;
 };
 
 export const saveFormData = async (formData) => {
-  const response = await axios.post(`${API_BASE_URL}/forms/saveData`, formData);
+  const response = await axios.post(
+    `${API_BASE_URL}/api/forms/saveData`,
+    formData
+  );
   return response.data;
 };
 
 export const getFormsByRole = async (role) => {
-  const response = await axios.get(`${API_BASE_URL}/forms/${role}`);
+  const response = await axios.get(`${API_BASE_URL}/api/forms/${role}`);
   return response.data;
 };
